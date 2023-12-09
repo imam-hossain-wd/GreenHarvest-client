536a20152ac6eb9f7cb0cd4f67cbc2dd29383cb2
imam-hossain1 <zunayedemon6t9@gmail.com>
2023-12-09 20:32:59 +0600

    navbar and footer added

--- src/pages/Shared/Footer/Footer.tsx
+++ src/pages/Shared/Footer/Footer.tsx
@@ -1,10 +1,81 @@
 
+import { Link } from "react-router-dom";
+
+import {
+  FacebookFilled,
+  GoogleCircleFilled,
+  MailFilled,
+  PhoneFilled,
+  TwitterOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
+
 const Footer = () => {
-    return (
-        <div>
-            <p>Footer.....</p>
+  return (
+    <div className=" bg-gray-100">
+      <footer className="p-10">
+        <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-16  justify-around ">
+          <div>
+            <div className="">
+              <div className="flex items-center">
+                <img
+                  className="w-14 h-14 rounded-full"
+                  src="https://i.ibb.co/863c3CY/car-service-logo.jpg"
+                  width={500}
+                  height={500}
+                  alt="car service logo"
+                />
+                <h3 className="text-xl hover:text-red-500 ml-2 font-semibold ">
+                Green <span className="text-red-400 ">Harvest</span>
+                </h3>
+              </div>
+              <div className="flex my-4">
+                <span className="text-2xl border-2 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 text-black duration-300 p-1  ml-2">
+                  <FacebookFilled />
+                </span>
+                <span className="text-2xl border-2 rounded p-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  text-black duration-300  ml-2">
+                  <WhatsAppOutlined />
+                </span>
+                <span className="text-2xl border-2 rounded p-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 text-black duration-300 ml-2">
+                  <GoogleCircleFilled />
+                </span>
+                <span className="text-2xl border-2 rounded p-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 text-black duration-300 ml-2">
+                  <TwitterOutlined />
+                </span>
+              </div>
+            </div>
+            <p className="hover:text-red-500">Chattogram , Bangladesh</p>
+          </div>
+          <div>
+            <div className="ml-2 text-lg mb-2">
+              <h2 className="text-xl font-semibold hover:text-red-500">
+                Contact Us
+              </h2>
+            </div>
+            <div>
+              <div className="flex items-center mb-1 hover:text-red-500 ">
+                <h4>
+                  <PhoneFilled />
+                </h4>
+                <Link to="tel:971568703512" className="ml-2 no-underline text-black hover:text-red-500">
+                  +00816524385
+                </Link>
+              </div>
+              <div className="flex items-center hover:text-red-500">
+                <h4>
+                  <MailFilled />
+                </h4>
+                <p className="ml-2">greenharvest@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+        <p className="text-center mt-2 hover:text-red-500">
+          Copyright © 2023 - All right reserved by Harvest
+        </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Shared/Navbar/Navbar.tsx
+++ src/pages/Shared/Navbar/Navbar.tsx
@@ -1,8 +1,132 @@
+import { Avatar, Button, Drawer, Dropdown, Space } from "antd";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { CloseOutlined, MenuOutlined, UserOutlined } from "@ant-design/icons";
+
+
+
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+      setOpen(true);
+    };
+
+    // const onClose = () => {
+    //   setOpen(false);
+    // };
+    
+    const items = [
+        {
+          key: "1",
+          label: (
+            <div>
+                <div className="flex flex-col">
+                  <Button type="text">
+                    <Link to={`/profile`}> Profile</Link>
+                  </Button>
+                  <Button  danger type="text">
+                    Log out
+                  </Button>
+                </div>
+            
+                <Button type="text">
+                  <Link to="/login"> Login</Link>
+                </Button>
+            </div>
+          ),
+        },
+      ];
+
+    const navMenuItems = (
+        <li className="list-none flex flex-col justify-center items-center lg:flex-row">
+          <Link
+            className="text-black  mt-2 lg:mt-0 lg:mr-3 no-underline "
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-black  mt-2 lg:mt-0 lg:mr-3 no-underline "
+            to="/service"
+          >
+            Contact Us
+          </Link>
+          <Link
+            className="text-black no-underline  mt-2 lg:mt-0 lg:mr-3    "
+            to="/contact"
+          >
+            Our Menu
+          </Link>
+          <Link
+            className="text-black no-underline mt-2 lg:mt-0 lg:mr-3"
+            to="/about"
+          >
+            About
+          </Link>
+          <Link
+            className="text-black no-underline mt-2 lg:mt-0 lg:mr-3  "
+            to="/booking"
+          >
+            Our Shop
+          </Link>
+        </li>
+      );
+
+
     return (
-        <div>
-            <p>Navbar.....</p>
-        </div>
+        <section className="relative z-50 mb-32 w-full">
+        <header className="fixed top-0 left-0 right-0  bg-white shadow-md mb-2">
+          <nav className="flex justify-between p-3 w-[90%] mx-auto">
+            <div className="flex items-center">
+              <Button className="lg:hidden mr-3 -ml-4 " onClick={showDrawer}>
+                <MenuOutlined />
+              </Button>
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://i.ibb.co/863c3CY/car-service-logo.jpg'"
+                width={500}
+                height={500}
+                alt="car service logo"
+              />
+              <h3 className="text-3xl font-bold text-black p-1">
+                Car<span className="text-[#f93e76]">Dev</span>
+              </h3>
+            </div>
+            <div className="flex items-center">
+              <ul className="hidden lg:flex">{navMenuItems}</ul>
+              <Dropdown menu={{ items }} className="">
+                <a className="-ml-10 lg:ml-2">
+                  <Space wrap size={24}>
+                    <Avatar
+                      className="text-[20px] "
+                      size="large"
+                      icon={<UserOutlined />}
+                    />
+                  </Space>
+                </a>
+              </Dropdown>
+            </div>
+          </nav>
+          <nav>
+            <Drawer
+              placement="left"
+              title={
+                <CloseOutlined
+                  className="ml-72 ml-5 hover:text-red-500 text-lg transition-all delay-300"
+                  onClick={() => setOpen(!open)}
+                  
+                />
+              }
+              open={open}
+              closable={false}
+            >
+              {navMenuItems}
+            </Drawer>
+          </nav>
+        </header>
+      </section>
     );
 };
 
